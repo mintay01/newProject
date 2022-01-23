5c1fa86a5caf718cf3bcd067ac90b9abcc94b1f9
mintay01 <86517412+mintay01@users.noreply.github.com>
2022-01-23 14:03:55 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 function myPassword() {
-    let showPassword = document.getElementsByClassName("password");
-    if (showPassword.type === "password") {
-        showPassword.type = "text";
-    } else {
-        showPassword.type = "password";
-    }
+  let showPassword = document.querySelectorAll("#password");
+  if (showPassword.type === "password") {
+    showPassword.type = "text";
+  } else {
+    showPassword.type = "password";
   }
+}
+
+//   dashboard script open
+const body = document.querySelector("body"),
+  sidebar = body.querySelector("nav"),
+  toggle = body.querySelector(".toggle"),
+  searchBtn = body.querySelector(".search-box"),
+  modeSwitch = body.querySelector(".toggle-switch"),
+  modeText = body.querySelector(".mode-text");
+
+toggle.addEventListener("click", () => {
+  sidebar.classList.toggle("close");
+});
+
+searchBtn.addEventListener("click", () => {
+  sidebar.classList.remove("close");
+});
+//   dashboard script close
